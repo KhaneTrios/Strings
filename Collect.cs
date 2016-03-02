76b1d08de206170b61cdbcf3037be4a64fcebda2
Chandler <KhaneTrios@outlook.com>
2016-03-01 20:37:02 -0500

    Added files via upload

---
+++ Collect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject Player = GameObject.Find("Player");
+		
+		//Call the trigger script
+		Player.GetComponent<triggers>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (triggers.collectibles >= 1) {
+			Destroy(gameObject);
+	}
+}
+}
